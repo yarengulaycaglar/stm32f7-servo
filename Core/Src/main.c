84045e997b281596ddc490024d72f1de791f9add
yarengulaycaglar <yarengulaycaglar@outlook.com>
2025-03-29 20:56:14 +0300

    updated fdcan initialize parameters

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -100,30 +100,36 @@ static void MX_FDCAN1_Init(void)
 	hfdcan1.Instance = FDCAN1;
 	hfdcan1.Init.FrameFormat = FDCAN_FRAME_CLASSIC;
 	hfdcan1.Init.Mode = FDCAN_MODE_NORMAL;
-	hfdcan1.Init.AutoRetransmission = ENABLE;
+	hfdcan1.Init.AutoRetransmission = DISABLE;
 	hfdcan1.Init.TransmitPause = DISABLE;
-	hfdcan1.Init.ProtocolException = ENABLE;
-	hfdcan1.Init.NominalPrescaler = 4;
-	hfdcan1.Init.NominalSyncJumpWidth = 5;
-	hfdcan1.Init.NominalTimeSeg1 = 14;
-	hfdcan1.Init.NominalTimeSeg2 = 5;
+	hfdcan1.Init.ProtocolException = DISABLE;
+	hfdcan1.Init.NominalPrescaler = 16;
+	hfdcan1.Init.NominalSyncJumpWidth = 1;
+	hfdcan1.Init.NominalTimeSeg1 = 2;
+	hfdcan1.Init.NominalTimeSeg2 = 2;
+	hfdcan1.Init.DataPrescaler = 1;
+	hfdcan1.Init.DataSyncJumpWidth = 1;
+	hfdcan1.Init.DataTimeSeg1 = 1;
+	hfdcan1.Init.DataTimeSeg2 = 1;
 	hfdcan1.Init.MessageRAMOffset = 0;
-	hfdcan1.Init.StdFiltersNbr = 1;
+	hfdcan1.Init.StdFiltersNbr = 0;
 	hfdcan1.Init.ExtFiltersNbr = 0;
-	hfdcan1.Init.RxFifo0ElmtsNbr = 8;
+	hfdcan1.Init.RxFifo0ElmtsNbr = 0;
 	hfdcan1.Init.RxFifo0ElmtSize = FDCAN_DATA_BYTES_8;
 	hfdcan1.Init.RxFifo1ElmtsNbr = 0;
 	hfdcan1.Init.RxFifo1ElmtSize = FDCAN_DATA_BYTES_8;
 	hfdcan1.Init.RxBuffersNbr = 0;
+	hfdcan1.Init.RxBufferSize = FDCAN_DATA_BYTES_8;
 	hfdcan1.Init.TxEventsNbr = 0;
 	hfdcan1.Init.TxBuffersNbr = 0;
-	hfdcan1.Init.TxFifoQueueElmtsNbr = 8;
+	hfdcan1.Init.TxFifoQueueElmtsNbr = 0;
 	hfdcan1.Init.TxFifoQueueMode = FDCAN_TX_FIFO_OPERATION;
 	hfdcan1.Init.TxElmtSize = FDCAN_DATA_BYTES_8;
 	if (HAL_FDCAN_Init(&hfdcan1) != HAL_OK)
 	{
 		Error_Handler();
 	}
+
 	/* FDCAN RX interrupt'ını etkinleştir */
 	HAL_FDCAN_ActivateNotification(&hfdcan1, FDCAN_IT_RX_FIFO0_NEW_MESSAGE, 0);
 
